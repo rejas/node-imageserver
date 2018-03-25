f0f14ed2ad14c637eaa830cd1df26320ef0a43a9
Veeck <github@veeck.de>
2018-03-25 18:19:49 +0200

    Refactor

--- config.js
+++ config.js
@@ -1,10 +1,12 @@
 // config.js
+const path = require('path');
+
 module.exports = {
     server: {
         port: 3000
     },
     images: {
-        cache: 'cache',
-        upload: 'public'
+        cache: path.join(__dirname, 'cache'),
+        upload: path.join(__dirname, 'public')
     }
 }
--- index.js
+++ index.js
@@ -1,67 +1,7 @@
-// Packages
-const fs = require('fs'),
-    restify = require('restify'),
-    path = require('path');
+const Server = require('./lib/server').Server;
+const config = require('./config.js');
+const winston = require('winston');
 
-// Configs
-const config = require('./config.js'),
-    info = require('./package.json');
+const server = new Server(config, winston);
 
-// Directories
-const cache = path.join(__dirname, 'cache'),
-    dir = path.join(__dirname, 'public');
-
-// Mime types
-const mime = {
-    gif: 'image/gif',
-    jpg: 'image/jpeg',
-    png: 'image/png',
-    svg: 'image/svg+xml'
-};
-
-// Server
-const server = restify.createServer({
-    name: info.name,
-    version: info.version
-});
-
-server.use(restify.plugins.acceptParser(server.acceptable));
-server.use(restify.plugins.queryParser());
-server.use(restify.plugins.bodyParser());
-
-server.get('/echo/:name', function (req, res, next) {
-    res.send(req.params);
-    return next();
-});
-
-server.get('/image/:name/:ext', function (req, res, next) {
-
-    var type = mime[req.params.ext] || 'text/plain';
-    res.header('Content-Type', type);
-
-    var targetFile = path.join(cache, req.params.name + '.' + req.params.ext),
-        readStream;
-
-    if (fs.existsSync(targetFile)) {
-
-        readStream = fs.createReadStream(targetFile);
-    } else {
-
-        var file = path.join(dir, req.params.name + '.' + req.params.ext);
-        fs.copyFileSync(file, targetFile);
-
-        readStream = fs.createReadStream(file);
-    }
-
-    readStream.pipe(res);
-    next();
-});
-
-server.listen(config.server.port, function () {
-
-    if (!fs.existsSync(cache)) {
-        fs.mkdirSync(cache);
-    }
-
-    winston.info('%s listening at %s', server.name, server.url);
-});
+server.run(process.env.PORT || config.server.port);
